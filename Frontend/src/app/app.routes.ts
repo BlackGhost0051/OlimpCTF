1c1574ceede9e287fb732812cbb35fe0aa57b344
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-05-15 15:54:02 +0200

    Add import

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {SigninComponent} from './components/signin/signin.component';
 import {SignupComponent} from './components/signup/signup.component';
 import {HomeComponent} from './components/home/home.component';
+import {WelcomeComponent} from './components/welcome/welcome.component';
 
 
 export const routes: Routes = [
