e183dfcd6a06c7b21fc73f92a0e9193a4f047c80
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-05-15 15:54:36 +0200

    Add welcome components

---
+++ Frontend/src/app/components/welcome/welcome-footer/welcome-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-footer',
+  imports: [],
+  templateUrl: './welcome-footer.component.html',
+  styleUrl: './welcome-footer.component.css'
+})
+export class WelcomeFooterComponent {
+
+}
---
+++ Frontend/src/app/components/welcome/welcome-header/welcome-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-header',
+  imports: [],
+  templateUrl: './welcome-header.component.html',
+  styleUrl: './welcome-header.component.css'
+})
+export class WelcomeHeaderComponent {
+
+}
