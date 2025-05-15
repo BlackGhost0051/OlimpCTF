aa49bed54ff0930cb9e61aea6b4bc7c37393e048
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-05-15 14:49:45 +0200

    Add login component

---
+++ Frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  standalone: true,
+  styleUrl: './login.component.css'
+})
+
+export class LoginComponent {
+
+}
