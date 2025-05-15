67b5909559bcd08bc7ec97cc7d1a7718134df5d5
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-05-15 15:30:32 +0200

    Mod rooters

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
+import {SigninComponent} from './components/signin/signin.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {HomeComponent} from './components/home/home.component';
+
 
 export const routes: Routes = [
   {
     path: '',
-    component: LoginComponent,
+    component: HomeComponent,
+  },
+  {
+    path: 'signIn',
+    component: SigninComponent,
+  },
+  {
+    path: 'signUp',
+    component: SignupComponent,
   }
 ];
