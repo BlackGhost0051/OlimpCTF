ce8be86d7522ad4b42610b8eacb67c3a63a722ef
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-05-16 20:32:52 +0200

    Add router

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {SigninComponent} from './components/signin/signin.component';
 import {SignupComponent} from './components/signup/signup.component';
 import {HomeComponent} from './components/home/home.component';
 import {WelcomeComponent} from './components/welcome/welcome.component';
+import {ProfileComponent} from './components/profile/profile.component';
 
 
 export const routes: Routes = [
@@ -17,5 +18,9 @@ export const routes: Routes = [
   {
     path: 'signUp',
     component: SignupComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
   }
 ];
