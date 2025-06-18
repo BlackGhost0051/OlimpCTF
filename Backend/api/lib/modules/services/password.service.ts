82ade5efec973cb98083af6ae9a84039fed7c109
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-18 21:13:15 +0200

    Mod PasswordService

--- Backend/api/lib/modules/services/password.service.ts
+++ Backend/api/lib/modules/services/password.service.ts
@@ -1,5 +1,14 @@
-class PasswordService{
+import bcrypt from 'bcrypt';
 
+class PasswordService{
+    async hashPassword(password: string): Promise<string>{
+        const saltRounds = 10;
+        return await bcrypt.hash(password, saltRounds);
+    }
+
+    async comparePassword(plainPassword: string, hashedPassword: string): Promise<boolean>{
+        return await bcrypt.compare(plainPassword, hashedPassword);
+    }
 }
 
 export default PasswordService;
