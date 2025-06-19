3e07a4170f0587e2fb9cf4085301f7da477cdaef
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-19 10:07:02 +0200

    Add connect to database

--- Backend/api/lib/config.ts
+++ Backend/api/lib/config.ts
@@ -1,3 +1,9 @@
 export const config = {
-    port: process.env.PORT || 5000
+    port: process.env.PORT || 5000,
+
+    db_host: process.env.DB_HOST || 'localhost',
+    db_port: parseInt(process.env.DB_PORT || '5432'),
+    db_user: process.env.DB_USER || 'user',
+    db_password: process.env.DB_PASSWORD || 'password',
+    db_database: process.env.DB_NAME || 'db',
 };
--- Backend/api/lib/modules/services/database.service.ts
+++ Backend/api/lib/modules/services/database.service.ts
@@ -1,7 +1,35 @@
 import PasswordService from "./password.service";
+import { Pool } from 'pg';
+import {config} from "../../config";
 
 class DatabaseService{
+    private pool: Pool;
 
+    constructor() {
+        this.pool = new Pool({
+            host: config.db_host,
+            port: config.db_port,
+            user: config.db_user,
+            password: config.db_password,
+            database: config.db_database,
+        });
+
+        this.pool.on('connect', () => {
+            console.log('Connected to PostgreSQL');
+        });
+
+        this.pool.on('error', (err) => {
+            console.error('Unexpected error on idle client', err);
+            process.exit(-1);
+        });
+
+        this.init();
+    }
+
+
+    async init(){
+        console.log("Init database");
+    }
 }
 
 export default DatabaseService;
