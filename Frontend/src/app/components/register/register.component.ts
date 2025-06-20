fbce3f6df9b5b9fa7c9ddd305468f254188792cc
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-20 10:45:37 +0200

    Add register form

--- Frontend/src/app/components/register/register.component.ts
+++ Frontend/src/app/components/register/register.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router , RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  errorMessage: string = "";
 
 }
