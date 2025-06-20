3846ade3fd822b867ffd418b1d1e200827808872
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-20 09:08:22 +0200

    Clear sign components

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import {SigninComponent} from './components/signin/signin.component';
-import {SignupComponent} from './components/signup/signup.component';
 import {HomeComponent} from './components/home/home.component';
 import {WelcomeComponent} from './components/welcome/welcome.component';
 import {ProfileComponent} from './components/profile/profile.component';
@@ -22,14 +20,6 @@ export const routes: Routes = [
     path: 'register',
     component: RegisterComponent,
   },
-  // {
-  //   path: 'signIn',
-  //   component: SigninComponent,
-  // },
-  // {
-  //   path: 'signUp',
-  //   component: SignupComponent,
-  // },
   {
     path: 'profile',
     component: ProfileComponent,
