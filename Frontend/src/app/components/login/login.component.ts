d887627362ed08f6abc3d2cbbd01bba86cd83ae1
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-20 10:45:28 +0200

    Mod form

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ RouterLink ],
+  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
   errorMessage: string = "";
 
