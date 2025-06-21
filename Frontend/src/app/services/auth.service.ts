2c96bc2121b6b98f885852cfdad79c4ae6575419
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-21 17:23:49 +0200

    Add isLoggedIn

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {JwtHelperService} from "@auth0/angular-jwt";
 import {DOCUMENT} from '@angular/common';
 
 @Injectable({
@@ -22,5 +23,16 @@ export class AuthService {
 
   logout(){
 
+  }
+
+  isLoggedIn(){
+    const localStorage = this.document.defaultView?.localStorage;
+    const jwtHelper = new JwtHelperService();
+    const token = localStorage?.getItem('token');
+
+    if (!token) {
+      return false;
+    }
+    return !(jwtHelper.isTokenExpired(token));
   }
 }
