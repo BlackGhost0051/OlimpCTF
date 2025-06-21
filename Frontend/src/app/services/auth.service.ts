7c3eee0ac582a91809d77d7464b73511b49c5b9e
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-21 17:13:34 +0200

    Mod service

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {DOCUMENT} from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
-  constructor() { }
+  private url = 'http://backend:5000/api';
+
+  constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) {}
+
+  login(){
+
+  }
+
+  register(){
+
+  }
+
+  logout(){
+
+  }
 }
