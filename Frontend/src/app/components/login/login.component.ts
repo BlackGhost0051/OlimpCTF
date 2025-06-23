ec04155ef8557ece4995d3fe9b95654eee93df23
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-23 19:56:30 +0200

    Add send info

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
+import {UserService} from '../../services/user/user.service';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,38 @@ import {Router, RouterLink} from '@angular/router';
 })
 
 export class LoginComponent {
-  errorMessage: string = "";
 
+  public login_info = {
+    login: '',
+    password: ''
+  }
+
+  public logged?: boolean;
+  public logout?: boolean;
+  public errorMessage?: string;
+
+
+  constructor(public userService: UserService, private router: Router) {}
+
+
+  login(){
+    this.errorMessage = undefined;
+    return this.userService.login(this.login_info).subscribe((result) => {
+      if (!result){
+        this.logged = false;
+      } else {
+        this.logout = false;
+
+        this.login_info = {
+          login: '',
+          password: ''
+        };
+        this.router.navigate(['/']);
+      }
+    },
+      (error) => {
+        this.errorMessage = 'Login or password is incorrect.';
+      }
+      );
+  }
 }
