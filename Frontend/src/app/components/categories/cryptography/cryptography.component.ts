120118652409015a2ee91fb9ed66189dad0f5203
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-25 14:47:28 +0200

    Generate new component

---
+++ Frontend/src/app/components/categories/cryptography/cryptography.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cryptography',
+  imports: [],
+  templateUrl: './cryptography.component.html',
+  styleUrl: './cryptography.component.css'
+})
+export class CryptographyComponent {
+
+}
