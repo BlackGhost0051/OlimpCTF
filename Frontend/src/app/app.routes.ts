42f881330c549f30ac6683c67121bdb529ef21ff
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-25 15:02:15 +0200

    Add router

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -5,27 +5,19 @@ import {ProfileComponent} from './components/profile/profile.component';
 import {CategoriesComponent} from './components/categories/categories.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import {WebComponent} from './components/categories/web/web.component';
+import {OsintComponent} from './components/categories/osint/osint.component';
+import {CryptographyComponent} from './components/categories/cryptography/cryptography.component';
 
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
-  },
-  {
-    path: 'categories',
-    component: CategoriesComponent,
-  }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'categories', component: CategoriesComponent },
+
+  { path: 'categories/web', component: WebComponent },
+  { path: 'categories/osint', component: OsintComponent },
+  { path: 'categories/cryptography', component: CryptographyComponent}
 ];
