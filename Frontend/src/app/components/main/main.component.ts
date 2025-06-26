e080bc01c31e7b4d2e1e9db36171c91e852ecc10
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-06-26 23:09:51 +0200

    Add main component

---
+++ Frontend/src/app/components/main/main.component.ts
@@ -0,0 +1,17 @@
+import {AfterViewInit, Component, ViewChild, ViewContainerRef} from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent implements AfterViewInit{
+  constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    this.router.navigate(['/home']);
+  }
+
+}
