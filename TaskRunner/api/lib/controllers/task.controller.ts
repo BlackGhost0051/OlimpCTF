179d46adbde2c8e4697078e3064dd24f5be85822
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-06 14:07:24 +0200

    Mod task controller

--- TaskRunner/api/lib/controllers/task.controller.ts
+++ TaskRunner/api/lib/controllers/task.controller.ts
@@ -1,15 +1,29 @@
 import Controller from "../interfaces/controller.interface";
 import { Router, Request, Response } from "express";
 
+import TaskService from "../modules/services/task.service";
+
 class TaskController implements Controller{
     public path: string = '/api/task';
     public router: Router = Router();
 
-    constructor() {
+    constructor(taskService: TaskService) {
         this.initializeRoutes();
     }
 
     private initializeRoutes(){
+        this.router.post(`${this.path}/verify_flag`, this.verifyFlag.bind(this));
+    }
+
+
+
+
+    private async verifyFlag(request: Request, response: Response){
+        const { flag, task_id } = request.body;
+
+        if(!flag || !task_id){
+            return response.status(400).json({ message : "Must be flag and task_id." });
+        }
     }
 }
 
