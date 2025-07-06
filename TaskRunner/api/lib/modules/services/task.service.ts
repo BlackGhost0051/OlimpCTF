f2aed095498ebc8bd39a6b8331cc59e0b3d64473
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-06 14:09:03 +0200

    Mod task service

--- TaskRunner/api/lib/modules/services/task.service.ts
+++ TaskRunner/api/lib/modules/services/task.service.ts
@@ -1,5 +1,10 @@
+import DatabaseService from "./database.service";
+import CryptographyService from "./cryptography.service";
+
 class TaskService {
 
+    constructor(databaseService: DatabaseService,
+                cryptographyService: CryptographyService) {}
 }
 
 export default TaskService;
