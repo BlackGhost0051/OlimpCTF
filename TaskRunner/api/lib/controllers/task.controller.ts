46f62b1c9d8b8dad60ffffe77454911de1f8823a
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-08 15:04:58 +0200

    Mod task controller

--- TaskRunner/api/lib/controllers/task.controller.ts
+++ TaskRunner/api/lib/controllers/task.controller.ts
@@ -6,6 +6,8 @@ import TaskService from "../modules/services/task.service";
 class TaskController implements Controller{
     public path: string = '/api/task';
     public router: Router = Router();
+    private taskService: TaskService;
+
 
     constructor(taskService: TaskService) {
         this.initializeRoutes();
@@ -13,6 +15,9 @@ class TaskController implements Controller{
 
     private initializeRoutes(){
         this.router.post(`${this.path}/verify_flag`, this.verifyFlag.bind(this));
+        this.router.post(`${this.path}/add`, this.addTask.bind(this));
+
+        this.router.put(`${this.path}/add`, this.updateTask.bind(this));
     }
 
 
@@ -24,6 +29,30 @@ class TaskController implements Controller{
         if(!flag || !task_id){
             return response.status(400).json({ message : "Must be flag and task_id." });
         }
+
+        const status: boolean = await this.taskService.verify_flag(task_id, flag);
+
+        return response.status(200).json({ status: status});
+    }
+
+    private async addTask(request: Request, response: Response){
+        const { flag, task_id } = request.body;
+
+        if(!flag || !task_id){
+            return response.status(400).json({ message : "Must be flag and task_id." });
+        }
+
+        await this.taskService.addTask(task_id, flag);
+    }
+
+    private async updateTask(request: Request, response: Response){
+        const { flag, task_id } = request.body;
+
+        if(!flag || !task_id){
+            return response.status(400).json({ message : "Must be flag and task_id." });
+        }
+
+        await this.taskService.updateTask(task_id, flag);
     }
 }
 
