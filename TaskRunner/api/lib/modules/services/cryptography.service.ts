037cdac898742b60a3755576fe6170e1622afd27
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-10 13:29:38 +0200

    Mod cryptography service

--- TaskRunner/api/lib/modules/services/cryptography.service.ts
+++ TaskRunner/api/lib/modules/services/cryptography.service.ts
@@ -1,7 +1,17 @@
 import * as crypto from 'crypto';
 
 class CryptographyService{
+    constructor() {}
 
+    public encryptFlag(flag: string): string {
+        const encryptedFlag = flag;
+        return encryptedFlag;
+    }
+
+    public decryptFlag(encryptedFlag: string): string {
+        const decryptedFlag = encryptedFlag;
+        return decryptedFlag;
+    }
 }
 
 export default CryptographyService;
