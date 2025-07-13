67fb9a713362da3ce4fe356c9d9a761aedb20c5a
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-13 18:51:08 +0200

    Mod task.service.ts

--- Backend/api/lib/modules/services/task.service.ts
+++ Backend/api/lib/modules/services/task.service.ts
@@ -1,5 +1,11 @@
-class TaskService {
+import { v4 as uuidv4 } from 'uuid';
+import DatabaseService from './database.service';
 
+class TaskService {
+    private databaseService: DatabaseService;
+    gatTaskInfo(){
+        return "title, if WEB ip, description, link to file...";
+    }
 }
 
 export default TaskService;
