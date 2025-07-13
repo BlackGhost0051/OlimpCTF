6d9f29f91d3b9b6832a29157366859f1b8a456bf
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-13 19:05:50 +0200

    Add task model

---
+++ Backend/api/lib/modules/models/task.model.ts
@@ -0,0 +1,9 @@
+export interface Task {
+    id?: string;
+    category: string;
+    title: string;
+    icon?: string;
+    difficulty?: string;
+    points?: number;
+    description?: string;
+}
