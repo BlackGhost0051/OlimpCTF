99496950ce26acdd24a8a9248578d8968ab2b22d
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-20 22:00:49 +0200

    Connect component

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -5,15 +5,13 @@ import {ProfileComponent} from './components/profile/profile.component';
 import {CategoriesComponent} from './components/categories/categories.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
-import {WebComponent} from './components/categories/web/web.component';
-import {OsintComponent} from './components/categories/osint/osint.component';
-import {CryptographyComponent} from './components/categories/cryptography/cryptography.component';
 import {MainComponent} from './components/main/main.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {AdminComponent} from './components/admin/admin.component';
 
 import { authGuard } from './services/auth/auth.guard';
 import {adminGuard} from './services/admin/admin.guard';
+import {CategoryComponent} from './components/category/category.component';
 
 
 export const routes: Routes = [
@@ -27,9 +25,7 @@ export const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'categories', component: CategoriesComponent },
 
-  { path: 'categories/web', component: WebComponent, canActivate: [authGuard] },
-  { path: 'categories/osint', component: OsintComponent, canActivate: [authGuard] },
-  { path: 'categories/cryptography', component: CryptographyComponent, canActivate: [authGuard]},
+  { path: 'category/:name', component: CategoryComponent/*, canActivate: [authGuard] */},
 
   { path: 'admin', component: AdminComponent, canActivate: [adminGuard]}
 ];
