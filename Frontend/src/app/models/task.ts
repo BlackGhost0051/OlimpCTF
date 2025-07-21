12ede890a25107dfc08372b66b6dd396d51ce692
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-21 16:24:31 +0200

    Add task model

---
+++ Frontend/src/app/models/task.ts
@@ -0,0 +1,15 @@
+export interface Task {
+  id: string;
+  title: string;
+  icon: string;
+
+  category: string;
+  author: string;
+  difficulty: string;
+  points: number;
+  description: string;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+  hint: any;
+}
