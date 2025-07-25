b67fa9543841e4f67c2b8f1db72eb8fe086f6e33
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-25 19:02:58 +0200

    Mod Task component

--- Frontend/src/app/components/task/task.component.ts
+++ Frontend/src/app/components/task/task.component.ts
@@ -1,14 +1,19 @@
 import {Component, Input} from '@angular/core';
 import {Task} from '../../models/task';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss'
 })
 export class TaskComponent{
   @Input() task: Task;
+
+  flagInput= "";
 
   constructor() {
   }
