67ff58ceebae8d11c9f64ade209d093f131c1f13
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-26 17:04:14 +0200

    Mod service

--- Frontend/src/app/services/task/task.service.ts
+++ Frontend/src/app/services/task/task.service.ts
@@ -1,14 +1,19 @@
 import {Injectable, Input} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private url = 'http://localhost:5001/';
 
+
+  constructor(private http: HttpClient) { }
+
+  // need verify user || user_id
   verifyFlag(id: string, flag:string){
-    return true;
+    return this.http.post(this.url + '/user/register', { id:id, flag:flag});
   }
 
   getCategories(){
