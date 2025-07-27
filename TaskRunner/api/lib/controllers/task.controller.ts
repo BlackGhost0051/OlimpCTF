61e0e92a56b0aeabc8a0e94d958028ec4563b305
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-07-27 18:00:41 +0200

    Add status in response

--- TaskRunner/api/lib/controllers/task.controller.ts
+++ TaskRunner/api/lib/controllers/task.controller.ts
@@ -28,26 +28,26 @@ class TaskController implements Controller{
         const { flag, task_id } = request.body;
 
         if(!flag || !task_id){
-            return response.status(400).json({ message : "Must be flag and task_id." });
+            return response.status(400).json({ status: false, message : "Must be flag and task_id." });
         }
 
         const status: boolean = await this.taskService.verify_flag(task_id, flag);
 
-        return response.status(200).json({ status: status});
+        return response.status(200).json({ status: status });
     }
 
     private async addTask(request: Request, response: Response){
         const { flag, task_id } = request.body;
 
         if(!flag || !task_id){
-            return response.status(400).json({ message : "Must be flag and task_id." });
+            return response.status(400).json({ status: false, message : "Must be flag and task_id." });
         }
 
         try{
             await this.taskService.addTask(task_id, flag);
-            return response.status(201).json({ message: "Task added successfully." });
+            return response.status(201).json({ status: true, message: "Task added successfully." });
         } catch (error){
-            return response.status(500).json({ message: error.message || "Failed to add task." });
+            return response.status(500).json({ status: false, message: "Failed to add task." });
         }
     }
 
@@ -55,14 +55,14 @@ class TaskController implements Controller{
         const { flag, task_id } = request.body;
 
         if(!flag || !task_id){
-            return response.status(400).json({ message : "Must be flag and task_id." });
+            return response.status(400).json({ status: false, message : "Must be flag and task_id." });
         }
 
         try {
             await this.taskService.updateTask(task_id, flag);
-            return response.status(200).json({ message: "Task updated successfully." });
+            return response.status(200).json({ status: true, message: "Task updated successfully." });
         } catch (err: any) {
-            return response.status(500).json({ message: err.message || "Failed to update task." });
+            return response.status(500).json({ status: false, message: "Failed to update task." });
         }
     }
 }
