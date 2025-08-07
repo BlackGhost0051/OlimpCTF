69b40cc6c0d53b3d5e4fa9e85dbe4abd094721ac
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-08-07 19:55:03 +0200

    Add LoggerService class for file-based logging of requests

---
+++ Backend/api/lib/modules/services/logger.service.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import {Request} from "express";
+
+class LoggerService {
+    private logFilePath: string;
+
+    constructor(){
+        this.logFilePath = path.join(__dirname, '../../logs/app.log');
+
+        const logDir = path.dirname(this.logFilePath);
+        if (!fs.existsSync(logDir)) {
+            fs.mkdirSync(logDir, { recursive: true });
+        }
+    }
+
+    public logToFile(request: Request){
+        const timestamp = new Date().toISOString();
+        const ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress || request.ip;
+        const userAgent = request.headers['user-agent'] || 'Unknown';
+        const method = request.method;
+        const url = request.url;
+
+        const fullMessage = `[${timestamp}] ${ip} ${method} ${url} ${userAgent}\n`;
+
+
+        fs.appendFileSync(this.logFilePath, fullMessage, 'utf8');
+    }
+
+}
+
+export default LoggerService;
