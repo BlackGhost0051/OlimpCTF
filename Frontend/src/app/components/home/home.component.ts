63321bd67ec9f54da1e50cda15f0e28337311c82
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-08-10 12:47:27 +0200

    Add header and add statistic container

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import {HomeHeaderComponent} from './home-header/home-header.component';
 import {FooterComponent} from '../global/footer/footer.component';
 
 
 @Component({
   selector: 'app-home',
-  imports: [HomeHeaderComponent, FooterComponent],
+  imports: [FooterComponent],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
