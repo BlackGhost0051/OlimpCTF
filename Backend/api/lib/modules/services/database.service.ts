3bc3054d0fe0c7b13562ee219de08a55733c78ae
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-08-20 20:02:09 +0200

    Mod users structure

--- Backend/api/lib/modules/services/database.service.ts
+++ Backend/api/lib/modules/services/database.service.ts
@@ -92,11 +92,13 @@ class DatabaseService{
         const query =`
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
+                name TEXT,
+                lastname TEXT,
                 login VARCHAR(50) NOT NULL UNIQUE,
+                password TEXT NOT NULL,
                 last_login TIMESTAMP,
                 email VARCHAR(150) NOT NULL UNIQUE,
                 email_verified BOOLEAN DEFAULT FALSE,
-                password TEXT NOT NULL,
                 isAdmin BOOLEAN DEFAULT FALSE,
                 bio TEXT,
                 created_at TIMESTAMP DEFAULT NOW()
