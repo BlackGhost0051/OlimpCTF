2ce5bb16e3077ff4c96dcd40cdfb3050c97b9207
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-08-21 20:49:38 +0200

    Add server.service.ts

---
+++ TaskRunner/api/lib/modules/services/server.service.ts
@@ -0,0 +1,16 @@
+class ServerService{
+
+    constructor() {}
+
+
+    public startTaskServer(task_id: string){
+
+    }
+
+
+    private startDokcerFile(task_id: string){
+
+    }
+}
+
+export default ServerService;
