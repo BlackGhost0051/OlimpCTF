9d8853cf87d88e5bedf932e0aabac9f77dcc16b4
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-08-24 21:33:57 +0200

    Add addTask and verifyFlag methods in task.runner.service.ts

--- Backend/api/lib/modules/services/task.runner.service.ts
+++ Backend/api/lib/modules/services/task.runner.service.ts
@@ -1,8 +1,42 @@
+import fetch from "node-fetch";
+
 class TaskRunnerService{
-    api_url: string = "";
+    api_url: string = "http://task-runner:5001";
 
-    addTask(id: string, flag:string){
+    async addTask(id: string, flag:string){
+        const response = await fetch(this.api_url + '/task', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                task_id: id,
+                flag: flag,
+            }),
+        });
 
+        if (!response.ok){
+            throw new Error("Failed to create task");
+        }
+        return await response.json();
+    }
+
+    async verifyFlag(id: string, flag:string){
+        const response = await fetch(this.api_url + '/verify_flag', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+               task_id: id,
+               flag: flag,
+            }),
+        });
+
+        if (!response.ok){
+            console.log(response.body);
+            throw new Error("Failed to verify task");
+        }
+
+        // TODO: verify logic
+        const data = await response.json() as { status: boolean };
+        return data.status;
     }
 }
 
