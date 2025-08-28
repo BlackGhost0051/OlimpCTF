081e2e5af99349bdb1c9dffa7be7fc75a88acba2
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-08-28 23:08:36 +0200

    Add getUsers method for admin

--- Frontend/src/app/services/admin/admin.service.ts
+++ Frontend/src/app/services/admin/admin.service.ts
@@ -1,23 +1,41 @@
 import {Inject, Injectable} from '@angular/core';
-import {catchError, map, Observable, of} from 'rxjs';
+import {catchError, map, Observable, of, tap} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {DOCUMENT} from '@angular/common';
+import {response} from 'express';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  private url = 'http://localhost:5000/api';
+  private url = 'http://localhost:5000/api/admin';
 
   constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) {}
 
+  getUsers() {
+    const localStorage = this.document.defaultView?.localStorage;
+    const token = localStorage?.getItem('token');
+    return this.http.get(this.url + '/users', {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      }
+    }).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  }
 
   isAdmin(): Observable<boolean> {
     const localStorage = this.document.defaultView?.localStorage;
     const token = localStorage?.getItem('token');
 
-    return this.http.post<{ status: boolean }>(this.url + '/admin', {}, {
+    return this.http.post<{ status: boolean }>(this.url, {}, {
       headers: {
         'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json',
