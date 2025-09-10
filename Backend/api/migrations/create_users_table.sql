e00d614c98b5806f8cfe3ef3e58c1a2103b8fe43
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-09-10 20:08:25 +0200

    Add migration create_users_table.sql

---
+++ Backend/api/migrations/create_users_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    lastname TEXT,
+    login VARCHAR(50) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    last_login TIMESTAMP,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    email_verified BOOLEAN DEFAULT FALSE,
+    isAdmin BOOLEAN DEFAULT FALSE,
+    bio TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
