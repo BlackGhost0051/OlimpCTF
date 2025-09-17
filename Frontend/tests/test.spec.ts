4b1553e25d5f86423d38f6e5a358f32af739dc74
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-09-17 20:27:35 +0200

    Update Playwright configuration to use a hardcoded base URL and bump @types/node version

--- Frontend/tests/test.spec.ts
+++ Frontend/tests/test.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 import config from '../playwright.config';
 
 test('test', async ({ page }) => {
-  await page.goto(config.baseURL);
+  await page.goto('http://localhost:4200');
 });
