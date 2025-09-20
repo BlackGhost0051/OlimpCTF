55b22656d7aea32507b56cbca143dd153ba1d708
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-09-20 19:56:08 +0200

    Change Task structure

--- Frontend/src/app/models/task.ts
+++ Frontend/src/app/models/task.ts
@@ -1,15 +1,15 @@
 export interface Task {
-  id: string;
+  id?: string;
   title: string;
   icon: string;
 
   category: string;
-  author: string;
+  author?: string;
   difficulty: string;
   points: number;
   description: string;
-  is_active: boolean;
-  created_at: string;
-  updated_at: string;
-  hints: any;
+  is_active?: boolean;
+  created_at?: string;
+  updated_at?: string;
+  hints?: any;
 }
