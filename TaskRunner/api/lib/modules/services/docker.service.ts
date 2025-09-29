3039b5834f81cd732a3b485922e666130335901d
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-09-29 20:20:29 +0200

    Mod docker.service.ts

--- TaskRunner/api/lib/modules/services/docker.service.ts
+++ TaskRunner/api/lib/modules/services/docker.service.ts
@@ -1,5 +1,21 @@
 class DockerService{
 
+    RESTART_TIME = 60 * 60000; // 60 minutes
+
+    constructor() {
+
+    }
+
+
+    startAllContainers(){
+
+    }
+
+    verifyContainerStatus(){
+
+    }
+
+
 }
 
 export default DockerService;
