cda28adcdf9fa4c2d65d9f7fd470012328200d0f
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-09-29 20:24:26 +0200

    Add migrations scripts in TaskRunner

---
+++ TaskRunner/api/migrations/sql/data/create_test_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+DB_HOST=$1
+DB_PORT=$2
+DB_USER=$3
+DB_PASSWORD=$4
+DB_NAME=$5
+
+export PGPASSWORD=$DB_PASSWORD
+
+#psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f "$(pwd)/sql/data/data_users_table.sql"
