1fe1cd952896066e310a237483e0c4930ff36db5
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-10-13 19:21:07 +0200

    Add task-form in admin page

--- Frontend/src/app/components/admin/admin.component.ts
+++ Frontend/src/app/components/admin/admin.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AddTaskFormComponent } from '../add-task-form/add-task-form.component';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [ AddTaskFormComponent ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
