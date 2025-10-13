46cd1081c7247f7426a19bede837ddefe3dc4645
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-10-13 19:10:26 +0200

    Remove form from home

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {FooterComponent} from '../global/footer/footer.component';
 import {ChallengeService} from '../../services/challenge/challenge.service';
-import {AddTaskFormComponent} from '../add-task-form/add-task-form.component';
 
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, AddTaskFormComponent],
+  imports: [FooterComponent],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
