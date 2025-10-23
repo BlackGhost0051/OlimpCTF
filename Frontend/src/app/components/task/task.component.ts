6547ddd70d8e10f6d6df32ab092006b9c9c82073
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-10-23 17:20:23 +0200

    Mod task component

--- Frontend/src/app/components/task/task.component.ts
+++ Frontend/src/app/components/task/task.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {Task} from '../../models/task';
 
 @Component({
@@ -9,5 +9,11 @@ import {Task} from '../../models/task';
 })
 export class TaskComponent{
   @Input() task!: Task;
+  @Output() taskClick = new EventEmitter<Task>();
+
   constructor() {}
+
+  onTaskClick() {
+    this.taskClick.emit(this.task);
+  }
 }
