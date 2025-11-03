f65f48e4ead2c43fd80600e49f629e6396543161
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-11-03 21:08:52 +0100

    Add app routes

--- AdminPanel/src/app/app.routes.ts
+++ AdminPanel/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {Login} from './components/login/login';
+import {AdminPanel} from './components/admin-panel/admin-panel';
+
+import {adminGuard} from './services/admin-guard';
+
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: '', component: AdminPanel, canActivate: [adminGuard]}
+];
