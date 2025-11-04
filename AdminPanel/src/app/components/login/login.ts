8969d2cb3f52f92504316babc056d339d69b3e36
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-11-04 22:52:01 +0100

    Add simple login page

--- AdminPanel/src/app/components/login/login.ts
+++ AdminPanel/src/app/components/login/login.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    FloatLabelModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  onSubmit() {
+    // Login logic will be implemented here
+  }
 }
