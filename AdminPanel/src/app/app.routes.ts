95d1bbfb723263f434373ed4de2c362879f1e770
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-11-04 22:52:37 +0100

    Mod routes

--- AdminPanel/src/app/app.routes.ts
+++ AdminPanel/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 
 import {Login} from './components/login/login';
 import {AdminPanel} from './components/admin-panel/admin-panel';
+import {FakeMaintenance} from './components/fake-maintenance/fake-maintenance';
 
 import {adminGuard} from './services/admin-guard';
 
+
+// TODO: NEED FIX ALL GO TO FakeMaintenance
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: '', component: AdminPanel, canActivate: [adminGuard]}
+  { path: '**', component: FakeMaintenance },
+  { path: 'auth_system_MB4IA61PEE9', component: Login },
+  { path: 'admin_console_MB4IA61PEE9', component: AdminPanel, canActivate: [adminGuard]},
+  { path: '**', component: FakeMaintenance },
 ];
