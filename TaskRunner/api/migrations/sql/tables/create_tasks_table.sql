6721f273037d3dfb54bb4a2a631bc403b2f7125f
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-11-15 20:33:06 +0100

    Change task runner table structure

--- TaskRunner/api/migrations/sql/tables/create_tasks_table.sql
+++ TaskRunner/api/migrations/sql/tables/create_tasks_table.sql
@@ -1,4 +1,8 @@
 CREATE TABLE IF NOT EXISTS tasks (
-    id TEXT NOT NULL,
-    flag TEXT NOT NULL
+    id TEXT PRIMARY KEY,
+    flag TEXT NOT NULL,
+    has_container BOOLEAN DEFAULT false,
+    has_files BOOLEAN DEFAULT false,
+    internal_port INTEGER,
+    created_at TIMESTAMP DEFAULT NOW()
 );
