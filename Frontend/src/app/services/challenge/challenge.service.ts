bfae50e9ec0197490dcb15e8db12895709fc312c
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-11-19 21:37:28 +0100

    Rm dont use import

--- Frontend/src/app/services/challenge/challenge.service.ts
+++ Frontend/src/app/services/challenge/challenge.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { Task } from '../../models/task';
 import {environment} from '../../../environments/environment';
 import {Observable} from 'rxjs';
 
