61b456e569cc95488f8d49e3123880bf8250dad0
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-11-30 20:30:21 +0100

    Add profile logic

--- Frontend/src/app/components/global/navbar/navbar.component.ts
+++ Frontend/src/app/components/global/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {ShortUserProfile} from '../../../models/user';
+import {UserService} from '../../../services/user/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,14 +13,41 @@ import {ShortUserProfile} from '../../../models/user';
 })
 export class NavbarComponent implements OnInit{
 
+  showProfileMenu: boolean = false;
   user: ShortUserProfile | undefined = undefined;
+
+
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
     this.loadUserInfo();
   }
 
+  profileMenuToggled(): void {
+    this.showProfileMenu = !this.showProfileMenu;
+  }
+
 
   loadUserInfo(): void {
+    this.userService.getUserProfile().subscribe({
+      next: (profile) => {
+        if(profile){
+          this.user = {
+            login: profile.login,
+            icon: profile.icon,
+          };
+        }
+      },
+      error: () => {
+        this.user = undefined;
+      }
+    });
+  }
 
+
+
+  onLogoutToggled(): void{
+    this.userService.logout();
+    this.loadUserInfo();
   }
 }
