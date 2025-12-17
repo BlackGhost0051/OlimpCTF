cb1dd7baa1f61fb9034d555f6c192d8072a707ba
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-12-17 20:09:47 +0100

    Add test configs and test library

--- Frontend/src/app/services/admin/admin.guard.ts
+++ Frontend/src/app/services/admin/admin.guard.ts
@@ -2,15 +2,8 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {AdminService} from './admin.service';
 
+// TODO: remove admin logic
 export const adminGuard: CanActivateFn = async (route, state) => {
-  const adminService = inject(AdminService);
   const router = inject(Router);
-
-  const isAdmin = await adminService.isAdmin().toPromise();
-
-  if (isAdmin) {
-    return true;
-  } else {
-    return router.createUrlTree(['/']);
-  }
+  return router.createUrlTree(['/']);
 };
