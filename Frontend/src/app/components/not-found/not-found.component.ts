0ebd8c756c99f1cafaa1d10c1aab22d57a7570a2
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-12-18 21:58:35 +0100

    Add style to not-found component

--- Frontend/src/app/components/not-found/not-found.component.ts
+++ Frontend/src/app/components/not-found/not-found.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ThreeDMountainsComponent } from '../three-d-mountains/three-d-mountains.component';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink, ThreeDMountainsComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
