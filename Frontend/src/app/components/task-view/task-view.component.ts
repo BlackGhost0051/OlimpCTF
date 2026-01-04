8a6f390a6dfe47e9b0ef05340ea00d74ab2287f5
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2026-01-04 21:33:43 +0100

    Correct TODOS

--- Frontend/src/app/components/task-view/task-view.component.ts
+++ Frontend/src/app/components/task-view/task-view.component.ts
@@ -77,7 +77,6 @@ export class TaskViewComponent implements OnInit{
       next: (response: any) => {
         if(response.status === true){
           alert("Correct flag!");
-          // TODO: Refresh tasks it must add flag what was resolved
 
           // TODO: Return data with completed flag in main task request
           if(this.hasContainer){
