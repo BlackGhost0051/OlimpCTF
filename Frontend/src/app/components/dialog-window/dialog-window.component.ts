9e61f7773db4fedf95343a7fc08af01962485cd1
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2026-01-07 21:46:47 +0100

    Add dialog component

---
+++ Frontend/src/app/components/dialog-window/dialog-window.component.ts
@@ -0,0 +1,21 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-window',
+  imports: [],
+  templateUrl: './dialog-window.component.html',
+  styleUrl: './dialog-window.component.scss',
+})
+export class DialogWindowComponent {
+
+  @Input() message: string = "";
+  @Output() submit = new EventEmitter<any>;
+  @Output() cancel = new EventEmitter<any>;
+
+  onSubmitToggled(): void{
+    this.submit.emit();
+  }
+  onCancelToggled(): void{
+    this.cancel.emit();
+  }
+}
